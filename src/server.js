2b43214b2595f80e0ca0d998e33bfdc54d163adb
Gabriel Arcanjo <gabriel520@live.com>
2019-04-11 19:45:12 -0300

    Alteração para o Heroku

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(cors());
  
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 
 io.on("connection", socket => {
@@ -34,6 +34,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
     
 server.listen(process.env.PORT || 3333);
